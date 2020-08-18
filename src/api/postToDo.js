5972072f6d2394c913766fef4c1596cb48fbf497
sschoe85 <s@bine-schoetensack.eu>
2020-08-18 16:30:28 +0200

    postToDo now really working :)

---
+++ src/api/postToDo.js
@@ -0,0 +1,11 @@
+export async function postToDo(toDo) {
+    const response = await fetch(
+      "http://localhost:3333/todos", {
+        method: "POST", headers: {"Content-Type": "application/json"
+      }, body: JSON.stringify(toDo)}
+
+    );
+    if (!response.ok) {
+      throw new Error(response);
+    }
+  }
