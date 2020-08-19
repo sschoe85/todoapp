3ffe5bff7e84ab97b2918657bb81e8f74059ee46
sschoe85 <s@bine-schoetensack.eu>
2020-08-19 12:28:23 +0200

    App modified

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import List from './components/List';
 import ListItem from './components/ListItem';
 import { postToDo } from './api/postToDo';
 import { deleteToDo } from './api/deleteToDo';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AddToDo from './pages/AddToDo';
 
 
 
@@ -20,7 +22,14 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <main>
+      <Router>
+        <Switch>
+          <Route path = "/add">
+            <AddToDo/>
+          </Route>
+        </Switch>
+        <Switch>
+          <Route path = "/home"> <main>
         {toDos?.map((todo)=>(<div>
             {todo.title}
         </div>))}
